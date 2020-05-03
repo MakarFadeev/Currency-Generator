13a338d08a9a300450f91cc533f571a66cffb528
Makar Fadeev <mafad2010@gmail.com>
2020-05-03 19:23:21 +0300

    add example_3

---
+++ example_3.pas
@@ -0,0 +1,9 @@
+﻿program example3;
+var a:integer;
+begin
+  writeln ('хотите получить денег? просто заплатите нам немного и получите в 10 раз больше!');
+  for a:=1 to 100 do
+  begin
+    writeln ('отправте 350 рублей');
+  end;
+end.
